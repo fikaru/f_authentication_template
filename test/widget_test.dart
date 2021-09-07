a84b95aabfa20fd049a5b314169ece69ec63b2c3
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2021-09-07 15:33:15 -0500

    Update with SignUp testing

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,7 @@
 import 'package:f_authentication_template/domain/controllers/authentication_controller.dart';
 import 'package:f_authentication_template/domain/use_case/authentication.dart';
 import 'package:f_authentication_template/ui/pages/authentication/login_page.dart';
+import 'package:f_authentication_template/ui/pages/authentication/signup_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
@@ -79,6 +80,54 @@ void main() {
     await tester.enterText(find.byKey(Key('loginPassord')), '123456');
 
     await tester.tap(find.byKey(Key('loginSubmit')));
+
+    await tester.pump();
+
+    expect(find.text('User problem'), findsOneWidget);
+  });
+
+  testWidgets('SignUp ok widget testing', (WidgetTester tester) async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(
+      GetMaterialApp(
+        home: SignUpPage(),
+      ),
+    );
+    await tester.pump();
+
+    expect(find.byKey(Key('signUpScaffold')), findsOneWidget);
+
+    await tester.enterText(find.byKey(Key('signUpEmail')), 'a@a.com');
+
+    await tester.enterText(find.byKey(Key('signUpPassord')), '123456');
+
+    await tester.tap(find.byKey(Key('signUpSubmit')));
+
+    await tester.pump();
+
+    expect(find.text('User ok'), findsOneWidget);
+  });
+
+  testWidgets('SignUp nok widget testing', (WidgetTester tester) async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(
+      GetMaterialApp(
+        home: SignUpPage(),
+      ),
+    );
+    await tester.pump();
+
+    expect(find.byKey(Key('signUpScaffold')), findsOneWidget);
+
+    await tester.enterText(find.byKey(Key('signUpEmail')), 'b@a.com');
+
+    await tester.enterText(find.byKey(Key('signUpPassord')), '123456');
+
+    await tester.tap(find.byKey(Key('signUpSubmit')));
 
     await tester.pump();
 
